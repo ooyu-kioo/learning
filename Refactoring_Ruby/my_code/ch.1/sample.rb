6507981e8021198155ddbb6228970d860f279b59
yuki.tashiro <yuki.tashiro@pixta.co.jp>
2020-05-11 20:09:47 +0900

    refactoring ruby 開始！

---
+++ Refactoring_Ruby/my_code/ch.1/sample.rb
@@ -0,0 +1,52 @@
+class Movie
+  REGULAR = 0
+  NEW_RELEASE = 1
+  CHILDRENS = 2
+
+  attr_reader :title
+  attr_accessor :price_code
+
+  def initialize(title, price_code)
+    @title, @price_code = title, price_code
+  end
+end
+
+class Rental
+  attr_reader :movie, :days_rented
+  
+  def initialize(movie, days_rented)
+    @movie, @days_rented = movie, days_rented
+  end
+end
+
+class Customer
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @rentals = []
+  end
+
+  def add_rental(arg)
+    @rentals << arg
+  end
+
+  def statement
+    @rentals.each do |element|
+    end
+    # ~
+  end
+
+  def amount_for(amount)
+    this._amount = 0
+      case amount.movie.price_code
+      when Movie::REGULAR
+        # ~
+      when Movie::NEW_RELEASE
+        # ~
+      when Movie::CHILDRENS
+        # ~
+      end
+  end
+
+end
