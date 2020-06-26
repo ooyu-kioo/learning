ec9e9eb64d15ca21630b126999e270e89d7673cd
yuki-tashiro <>
2020-06-26 18:38:45 +0900

    refavtoring ruby 終わり！

---
+++ Refactoring_Ruby/my_code/ch.12_大規模なリファクタリング/12.1_複合的な継承階層の分割(Tease ApartInheritance).rb
@@ -0,0 +1,14 @@
+"""
+- Deal
+  - Active_Deal
+    - Tabular_Active_Deal
+  - Passive_Deal
+    - Tablar_Passive_Deal
+
+積極取引と消極取引があり、そこにtable(表)表示を加えようってなってさらにサブクラスがぶら下がってる
+=> サブクラスにサブクラスがぶら下がっているような状態
+
+こうなると、新たに取引のclassを追加するのはしんどくなる
+"""
+
+サンプルなし
